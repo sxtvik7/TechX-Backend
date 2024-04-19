75fa9a28abd8b2302c71381a68ba9f4fdbd8fa86
Satvik <Satvikgovale7@gmail.com>
2024-04-19 16:49:44 +0530

    specifying origin

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ const port = process.env.PORT;
 app.use(express.json())
 
 app.use(cors({
+  origin:[process.env.FRONTEND_URL],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
--- dataBase/data.js
+++ dataBase/data.js
@@ -3,11 +3,11 @@ const mySql = require("mysql");
 // Create a connection pool to the MySQL database
 const connection = mySql.createPool({
   connectionLimit: 10,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "techx",
-  port: 3308,
+  host: "sql6.freesqldatabase.com",
+  user: "sql6700329",
+  password: "xlsuMisTSM",
+  database: "sql6700329",
+  port: 3306,
 });
 
 module.exports = connection;
