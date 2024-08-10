59c713f886bebf18eab4bd9d0d99165500b9cfea
Satvik <Satvikgovale7@gmail.com>
2024-08-10 22:49:22 +0530

    change DB

--- dataBase/data.js
+++ dataBase/data.js
@@ -3,9 +3,9 @@ const mySql = require("mysql");
 // Create a connection pool to the MySQL database
 const connection = mySql.createPool({
   connectionLimit: 10,
-  host: "10.10.10.111",
-  user: "rstuym8um",
-  password: "e22abc47",
+  host: "sql12.freesqldatabase.com",
+  user: "sql12725179",
+  password: "LK9zQ5Zi9c",
   database: "test",
   port: 3306,
 });
